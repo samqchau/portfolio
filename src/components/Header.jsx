2beaac9bad642788667b160f65dc8800f610fe88
samqchau <67344952+samqchau@users.noreply.github.com>
2021-07-06 14:24:53 -0700

    portfolio 7/6

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../styles/header.scss';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-nav'>
+        <div className='header-nav-socials'>
+          <div className='icon-container' title='Dribble' alt='Dribble'>
+            <img className='icon' src='/pngs/dribble_64.png' alt='Github' />
+          </div>
+          <div className='icon-container' title='LinkedIn' alt='LinkedIn'>
+            <img className='icon' src='/pngs/linked_in_64.png' alt='LinkedIn' />
+          </div>
+          <div className='icon-container' title='Github' alt='Github'>
+            <img className='gh-icon' src='/pngs/gh_64.png' alt='Github' />
+          </div>
+        </div>
+      </div>
+      <div className='header-nav-items'>
+        <div className='header-nav-item'>Home</div>
+        <div className='header-nav-item'>About</div>
+        <div className='header-nav-item'>Projects</div>
+        <div className='header-nav-item'>Contact</div> |
+      </div>
+    </div>
+  );
+};
+
+export default Header;
