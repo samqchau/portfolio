77e2f7ae7b273af42657173622c76d46ee2b3df7
samqchau <67344952+samqchau@users.noreply.github.com>
2021-07-09 12:25:46 -0700

    footer-social-icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import '../styles/footer.scss';
 
 const Footer = () => {
-  return               <div className='screen-footer'>
-                <div className='screen-footer-content'>
-                  <div className='footer-content-left'>
-                    Designed By: Samuel Chau
-                  </div>
-                  <div className='footer-content-right'>
-                    Github Facebook LinkedIn
-                  </div>
-                </div>
-              </div>
-;
+  return (
+    <div className='screen-footer'>
+      <div className='screen-footer-content'>
+        <div className='footer-content-left'>Designed By: Samuel Chau</div>
+        <div className='footer-content-right'>
+          <i className='footer-social-icon fab fa-instagram'></i>
+          <i className='footer-social-icon fab fa-facebook-square'></i>
+          <i className='footer-social-icon fab fa-twitter'></i>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
